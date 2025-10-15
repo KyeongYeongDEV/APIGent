ade42833a2f5e7cf8d9ce1854584e3185c97707d
최경영 <yeong7098@naver.com>
2025-10-15 18:53:45 +0900

    Init

---
+++ src/main/kotlin/com/github/KyeongYeongDEV/apigent/ApiGentApplication.kt
@@ -0,0 +1,11 @@
+package com.github.KyeongYeongDEV.apigent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGentApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGentApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/KyeongYeongDEV/apigent/ApiGentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.KyeongYeongDEV.apigent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
