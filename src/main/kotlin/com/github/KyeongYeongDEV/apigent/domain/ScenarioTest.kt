e60e4f50b569a25b54a9e5a57c50a779fa43fb3e
최경영 <yeong7098@naver.com>
2025-10-19 17:10:09 +0900

    Feat : Entity 생성

--- src/main/kotlin/com/github/KyeongYeongDEV/apigent/domain/ScenarioTest.kt
+++ src/main/kotlin/com/github/KyeongYeongDEV/apigent/domain/ScenarioTest.kt
@@ -1,4 +1,29 @@
 package com.github.KyeongYeongDEV.apigent.domain
 
-class ScenarioTest {
-}
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import java.time.LocalDateTime
+
+
+data class ScenarioTest(
+    @Id
+    val id : Long? = null,
+
+    @Column("swagger_url")
+    val swaggerUrl : String,
+
+    @Column("user_scenario")
+    val userScenario : String,
+
+    @Column("ai_plan")
+    val aiPlan : String? = null,
+
+    var status: String,
+
+    @Column("result_details")
+    var resultDetails: String? = null,
+    @Column("duration_ms")
+    var durationMs: Long? = null,
+    @Column("created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)
